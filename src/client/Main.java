9870452b43b762bc9204884058a7bcb859ab76e6
SERGIO LOPES JUNIOR <200026817@ucsal.br>
2026-05-11 21:05:16 -0300

    Initial commit

---
+++ src/client/Main.java
@@ -0,0 +1,10 @@
+package client;
+
+
+import shared.Color;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Color.VERDE + "Verde" + Color.AMARELO + "Amarela");
+    }
+}
